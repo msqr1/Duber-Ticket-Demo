62dd677f4be74be0a9f513b93bb729ee97573547
Schljin <schljin@schljin.schljin.local>
2023-10-09 21:52:40 -0700

    Update ticket page

---
+++ tickets.py
@@ -0,0 +1,44 @@
+from kivymd.uix.screenmanager import ScreenManager
+from kivymd.uix.screen import *
+from kivymd.uix.pickers import *
+from kivymd.app import MDApp
+from kivy.lang.builder import Builder
+class nbook(MDScreen): #Regular ticket screen
+    def savedate(self,a,b,c):
+        self.ids.rdbutton.text = str(b)
+    def savetime(self,a,b):
+        self.ids.rtbutton.text = str(b)
+    def pickdate(self):
+        date_dialog = MDDatePicker()
+        date_dialog.bind(on_save=self.savedate)
+        date_dialog.open()
+    def picktime(self):
+        time_dialog = MDTimePicker()
+        time_dialog.bind(on_save=self.savetime)
+        time_dialog.open()
+class rbook(MDScreen): #Recurring ticket screen
+    def savedate(self,a,b,c):
+        self.ids.rdbutton.text = str(b)
+        print(self.get_root_window().ids)
+    def savetime(self,a,b):
+        self.ids.rtbutton.text = str(b)
+    def pickdate(self):
+        date_dialog = MDDatePicker()
+        date_dialog.bind(on_save=self.savedate)
+        date_dialog.open()
+    def picktime(self):
+        time_dialog = MDTimePicker()
+        time_dialog.bind(on_save=self.savetime)
+        time_dialog.open()
+class nbook2(MDScreen): #Next regular ticket screen
+    pass
+class Duber(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Purple"  # "Purple", "Red"
+        sm = ScreenManager()
+        sm.add_widget(nbook(name="nbook"))
+        sm.add_widget(rbook(name="rbook"))
+        return sm
+Builder.load_string("")
+Duber().run()
