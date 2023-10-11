8e9ffa32c381242dcbd30cc3b6721bcfc4f626df
MisterSquareOne <134968689+msqr1@users.noreply.github.com>
2023-10-10 19:03:10 -0700

    Update tickets.py

--- tickets.py
+++ tickets.py
@@ -1,44 +1,58 @@
 from kivymd.uix.screenmanager import ScreenManager
+from kivymd.theming import ThemeManager
 from kivymd.uix.screen import *
 from kivymd.uix.pickers import *
 from kivymd.app import MDApp
-from kivy.lang.builder import Builder
-class nbook(MDScreen): #Regular ticket screen
-    def savedate(self,a,b,c):
+import datetime
+usrconfig = {
+            'st': None, #start location
+            'sd': [], #start date, repetition amount (0 for regular ticket)
+            'sl': None, #start time
+            'dst': None, #destination
+            'ppl': None #people amount
+        }
+class book1:
+    def savedate(self,a,b,c):        
         self.ids.rdbutton.text = str(b)
     def savetime(self,a,b):
+        global usrconfig
         self.ids.rtbutton.text = str(b)
+        usrconfig['st'] = b
     def pickdate(self):
         date_dialog = MDDatePicker()
         date_dialog.bind(on_save=self.savedate)
         date_dialog.open()
     def picktime(self):
-        time_dialog = MDTimePicker()
+        time_dialog = MDTimePicker(
+            primary_color="black",
+            text_color="black"
+        )
+        if(usrconfig['st']):
+            time_dialog.set_time(usrconfig['st'] if usrconfig['st'].hour != 0 else datetime.time.fromisoformat("12:00:00"))
         time_dialog.bind(on_save=self.savetime)
         time_dialog.open()
-class rbook(MDScreen): #Recurring ticket screen
+class book2:
+    pass
+class nbook(MDScreen,book1): #Regular ticket screen 
+    pass
+class rbook(MDScreen,book1): #Recurring ticket screen
     def savedate(self,a,b,c):
-        self.ids.rdbutton.text = str(b)
-        print(self.get_root_window().ids)
-    def savetime(self,a,b):
-        self.ids.rtbutton.text = str(b)
+        print(b,c)
+        self.ids.rdbutton.text = str(str(b).replace("-","/") + "  -  " + str(c[len(c)-1]).replace("-","/"))
     def pickdate(self):
-        date_dialog = MDDatePicker()
+        date_dialog = MDDatePicker(mode="range")
         date_dialog.bind(on_save=self.savedate)
         date_dialog.open()
-    def picktime(self):
-        time_dialog = MDTimePicker()
-        time_dialog.bind(on_save=self.savetime)
-        time_dialog.open()
-class nbook2(MDScreen): #Next regular ticket screen
+class nbook2(MDScreen,book2): #Next regular ticket screen
+    pass
+class rbook2(MDScreen,book2): #Next recurring ticket screen
     pass
 class Duber(MDApp):
     def build(self):
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Yellow"
+        self.theme_cls.theme_style = "Dark" #Dark theme
+        self.theme_cls.primary_palette = "Green"  #Green accent
         sm = ScreenManager()
         sm.add_widget(nbook(name="nbook"))
         sm.add_widget(rbook(name="rbook"))
         return sm
-Builder.load_string("")
 Duber().run()
